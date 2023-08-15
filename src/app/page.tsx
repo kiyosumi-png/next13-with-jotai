83f649a079c08e42c3c652861c48298e6fe48579
kiyosumi-png <shoudaoqingcheng3@gmail.com>
2023-08-16 06:49:31 +0900

    basic atom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { atom, useAtom } from "jotai";
+
+const counter = atom(0);
+
 export default function Home() {
-  return <main></main>;
+  const [count, setCount] = useAtom(counter);
+  const onClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  return (
+    <main>
+      <p>{count}</p>
+      <button onClick={onClick}>+1</button>
+    </main>
+  );
 }
